921a777d44cd56f992d3cd00c23ad2a7663a3305
AndreyMosiakov <mosiakoff@yandex.ru>
2023-12-15 14:41:17 +0300

    добавлены изменения после 4 лекции

--- selenium2/BaseApp.py
+++ selenium2/BaseApp.py
@@ -1,3 +1,5 @@
+import logging
+
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -8,16 +10,34 @@ class BasePage:
         self.base_url = "https://test-stand.gb.ru"
 
     def find_element(self, locator, time=10):
-        return WebDriverWait(self.driver, time).until(EC.presence_of_element_located(locator),
-                                                      message=f"Can't find element by locator{locator}")
+        try:
+            element = WebDriverWait(self.driver, time).until(EC.presence_of_element_located(locator),
+                                                   message=f"Can't find element by locator{locator}")
+        except:
+            logging.exception("find element exception")
+            element = None
+        return element
 
     def get_element_property(self, locator, property):
         element = self.find_element(locator)
-        return element.value_of_css_property(property)
+        if element:
+            return element.value_of_css_property(property)
+        else:
+            logging.error(f"Property {property} not found in element with locator{locator}")
+            return None
 
     def go_to_site(self):
-        return self.driver.get(self.base_url)
+        try:
+            start_browsing = self.driver.get(self.base_url)
+        except:
+            logging.exception("Exception while open site")
+            start_browsing = None
+        return start_browsing
 
     def is_alert(self):
-        alert = self.driver.switch_to.alert.text
-        return alert
+        try:
+            alert = self.driver.switch_to.alert
+            return alert.text
+        except:
+            logging.exception("Exception with alert")
+            return None
